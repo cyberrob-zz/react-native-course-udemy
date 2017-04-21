47bde0f69a8b4d536c5d0d078ca60bd1fa54a157
robertwang <robertwang.tw@gmail.com>
2017-04-22 00:33:05 +0800

    lecture 46

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,13 +1,66 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Image, Text } from 'react-native';
 import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+  // extract these properties from album that from props
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    thumbnailStyle,
+    infoStyle,
+    titleStyle,
+    artworkStyle
+  } = styles;
+
   return (
     <Card>
-      <Text>{props.album.title}</Text>
+      <CardSection>
+        <View>
+          <Image
+            style={thumbnailStyle}
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={infoStyle}>
+          <Text style={titleStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          style={artworkStyle}
+          source={{ uri: image }}
+        />
+      </CardSection>
     </Card>
   );
+};
+
+const styles = {
+  thumbnailStyle: {
+    width: 50,
+    height: 50,
+    borderColor: '#fff',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginRight: 5
+  },
+  infoStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    //backgroundColor: 'yellow',
+    height: 50
+  },
+  titleStyle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  artworkStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
 };
 
 export default AlbumDetail;
--- src/components/Card.js
+++ src/components/Card.js
@@ -23,8 +23,6 @@ const styles = {
     marginLeft: 5,
     marginRight: 5,
     marginTop: 10,
-    padding: 10,
-
   }
 };
 
